892050ac954aff8cd846fd814c585a5a8b9065bf
Klinton Lee <klintonlee92@gmail.com>
2021-11-15 02:14:50 -0300

    Implementation of mongodb and redis containers with simple instance

--- src/common/config.js
+++ src/common/config.js
@@ -4,6 +4,19 @@ const config = {
     port: process.env.APP_PORT,
     errorLogPathFile: process.env.ERROR_LOG_PATH_FILE || null,
     combinedLogPathFile: process.env.COMBINED_LOG_PATH_FILE || null
+  },
+  database: {
+    mongo: {
+      host: process.env.MONGODB_HOST,
+      port: process.env.MONGODB_PORT,
+      user: process.env.MONGODB_USER,
+      pass: process.env.MONGODB_PASS
+    },
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS
+    }
   }
 }
 
